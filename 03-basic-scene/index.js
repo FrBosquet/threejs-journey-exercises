c3505a6612c514a1a5974460ce0d8616791bfcf4
Fran Bosquet <frbosquet@gmail.com>
2021-12-26 12:33:39 +0100

    complete 03

---
+++ 03-basic-scene/index.js
@@ -0,0 +1,25 @@
+console.log('script loaded')
+
+const scene = new THREE.Scene()
+
+const geometry = new THREE.BoxGeometry(1, 1, 1)
+const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
+const mesh = new THREE.Mesh(geometry, material)
+
+scene.add(mesh)
+
+const sizes = { w: 800, h: 600 }
+
+const camera = new THREE.PerspectiveCamera(75, sizes.w / sizes.h)
+camera.position.z = 3
+camera.position.x = 1
+scene.add(camera)
+
+const canvas = document.querySelector('#webgl')
+
+const renderer = new THREE.WebGLRenderer({
+  canvas,
+})
+
+renderer.setSize(sizes.w, sizes.h)
+renderer.render(scene, camera)
