7a461db8dbb58375c8b43a6da52db5b87d97e4ee
Fran Bosquet <frbosquet@gmail.com>
2023-01-30 20:28:57 +0100

    finish scroll animations

--- 20-scroll-based-animation/src/script.js
+++ 20-scroll-based-animation/src/script.js
@@ -1,5 +1,10 @@
-import * as THREE from 'three'
+import gsap from 'gsap'
 import * as dat from 'lil-gui'
+import * as THREE from 'three'
+import { BufferAttribute, BufferGeometry, DirectionalLight, Group, MeshToonMaterial, NearestFilter, Points, PointsMaterial, TextureLoader } from 'three'
+
+const OBJECT_DIST = 10
+
 
 /**
  * Debug
@@ -12,6 +17,10 @@ const parameters = {
 
 gui
     .addColor(parameters, 'materialColor')
+    .onChange(() => {
+        material.color.set(parameters.materialColor)
+        particles.material.color.set(parameters.materialColor)
+    })
 
 /**
  * Base
@@ -22,14 +31,74 @@ const canvas = document.querySelector('canvas.webgl')
 // Scene
 const scene = new THREE.Scene()
 
-/**
- * Test cube
- */
-const cube = new THREE.Mesh(
-    new THREE.BoxGeometry(1, 1, 1),
-    new THREE.MeshBasicMaterial({ color: '#ff0000' })
-)
-scene.add(cube)
+// Textures
+const textureLoader = new TextureLoader()
+const gradientTexture = textureLoader.load('textures/gradients/3.jpg')
+gradientTexture.magFilter = NearestFilter
+
+/* 
+    Particles
+*/
+const particlesCount = 200
+const positions = new Float32Array(particlesCount * 3)
+
+for (let i = 0; i < particlesCount; i++) {
+    positions[i * 3 + 0] = (Math.random() - 0.5) * 5
+    positions[i * 3 + 1] = - OBJECT_DIST * 0.4 + Math.random() * OBJECT_DIST * 3
+    positions[i * 3 + 2] = (Math.random() - 0.5) * 5
+}
+
+const particlesGeometry = new BufferGeometry()
+particlesGeometry.setAttribute('position', new BufferAttribute(positions, 3))
+
+const particlesMaterial = new PointsMaterial({
+    color: parameters.materialColor,
+    sizeAttenuation: true,
+    size: 0.03
+})
+
+const particles = new Points(particlesGeometry, particlesMaterial)
+scene.add(particles)
+
+/* 
+    Objects
+*/
+const material = new MeshToonMaterial({
+    color: parameters.materialColor,
+    gradientMap: gradientTexture
+})
+
+const meshes = [
+    new THREE.Mesh(
+        new THREE.TorusGeometry(1, 0.4, 16, 60),
+        material
+    ),
+    new THREE.Mesh(
+        new THREE.ConeGeometry(1, 2, 32),
+        material
+    ),
+    new THREE.Mesh(
+        new THREE.TorusKnotGeometry(.8, .35, 100, 16),
+        material
+    ),
+]
+
+
+scene.add(...meshes)
+
+meshes.forEach((m, i) => {
+    m.position.y = OBJECT_DIST * i
+    m.position.x = 1.5 * (i % 2 ? - 1 : 1)
+    m.scale.set(1, 1, 1)
+})
+
+/* 
+    Light
+*/
+const light = new DirectionalLight({ color: 0xffffff })
+light.position.set(1, 1, 0)
+
+scene.add(light)
 
 /**
  * Sizes
@@ -39,8 +108,9 @@ const sizes = {
     height: window.innerHeight
 }
 
-window.addEventListener('resize', () =>
-{
+let currentSection = 0
+
+window.addEventListener('resize', () => {
     // Update sizes
     sizes.width = window.innerWidth
     sizes.height = window.innerHeight
@@ -52,33 +122,82 @@ window.addEventListener('resize', () =>
     // Update renderer
     renderer.setSize(sizes.width, sizes.height)
     renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+
+    scroll = window.scrollY / window.innerHeight
+})
+
+const cursor = { x: 0, y: 0 }
+window.addEventListener('mousemove', (event) => {
+    cursor.x = event.clientX / sizes.width
+    cursor.y = event.clientY / sizes.height
 })
 
 /**
  * Camera
  */
 // Base camera
+const cameraGroup = new Group()
 const camera = new THREE.PerspectiveCamera(35, sizes.width / sizes.height, 0.1, 100)
 camera.position.z = 6
-scene.add(camera)
+cameraGroup.add(camera)
+scene.add(cameraGroup)
+
 
 /**
  * Renderer
  */
 const renderer = new THREE.WebGLRenderer({
-    canvas: canvas
+    canvas: canvas,
+    alpha: true
 })
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+
+/* 
+    Scroll
+*/
+let scroll = window.scrollY / window.innerHeight
+window.addEventListener('scroll', () => {
+    scroll = window.scrollY / window.innerHeight
+
+    const newSection = Math.floor(scroll)
+
+    if (newSection != currentSection) {
+        gsap.to(meshes[newSection].rotation, {
+            duration: 1.5,
+            ease: 'power2.inOut',
+            x: '+=6',
+            y: '+=3'
+        })
+
+
+        currentSection = newSection
+    }
+})
+
 
 /**
  * Animate
  */
 const clock = new THREE.Clock()
+let prevTime = 0
 
-const tick = () =>
-{
+const tick = () => {
+    // Animate camera
     const elapsedTime = clock.getElapsedTime()
+    const deltaTime = elapsedTime - prevTime
+    prevTime = elapsedTime
+
+    camera.position.y = scroll * OBJECT_DIST
+
+    const parallaxX = (cursor.x - 0.5) * 1
+    const parallaxY = - (cursor.y - 0.5) * 1
+    cameraGroup.position.y += (parallaxY - cameraGroup.position.y) * deltaTime
+    cameraGroup.position.x += (parallaxX - cameraGroup.position.x) * deltaTime
+
+    meshes.forEach(m => {
+        m.rotation.x += deltaTime * 0.3
+    })
 
     // Render
     renderer.render(scene, camera)
