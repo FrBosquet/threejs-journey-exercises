a7140323e9deede3b6dfaf9b61a271ba3d3a0211
Fran Bosquet <frbosquet@gmail.com>
2023-03-07 19:42:18 +0100

    init structuring

--- 28-structuring/src/script.js
+++ 28-structuring/src/script.js
@@ -1,7 +1,10 @@
+import * as dat from 'lil-gui'
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
-import * as dat from 'lil-gui'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
+import test from './test'
+
+console.log(test);
 
 /**
  * Loaders
